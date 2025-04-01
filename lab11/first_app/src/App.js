c6c5e07546dd23cebb92a117f4c014899cb4fa56
IdressBaguaei56 <Idress.Baguaei56@student.qcc.cuny.edu>
2025-04-01 19:36:31 +0000

    start lab 11

---
+++ lab11/first_app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1> Idress Baguaei </h1>
+      <p>This is my first REACTJS app</p>
+    </div>
+
+  );
+}
+
+export default App;
