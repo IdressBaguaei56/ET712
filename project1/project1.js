ded1c4ec3483f5af7fc5eca18f829f1a4f4aee81
Idress Baguaei <Idress.Baguaei56@student.qcc.cuny.edu>
2025-04-01 11:31:08 -0400

    update project 1

--- project1/project1.js
+++ project1/project1.js
@@ -1,13 +1,11 @@
-// example 8 mouseover event 
-// change the background color of a div when the mouse hover the element 
-// collect element 
-let divexample8 = document.querySelector(".divexample8")
-divexample8.onmouseout = function(){
-    let randred = Math.floor(Math.random()*255)
-    let randgreen = Math.floor(Math.random()*255)
-    let randblue = Math.floor(Math.random()*255)
-    divexample8.style.backgroundColor = `rgb(${randred},${randgreen},${randblue})`
-}
+// This is the color changing wheel code
+let divexample8 = document.querySelector(".divexample8");
+divexample8.onmouseout = function() {
+    let randred = Math.floor(Math.random() * 255);
+    let randgreen = Math.floor(Math.random() * 255);
+    let randblue = Math.floor(Math.random() * 255);
+    divexample8.style.backgroundColor = `rgb(${randred},${randgreen},${randblue})`;
+};
 // color changing part
 function changeColor() {
     const colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"];
@@ -18,3 +16,21 @@ function changeColor() {
     circle.style.backgroundColor = newColor.toLowerCase();
     circle.textContent = newColor;
 }
+// select elements 
+let shape = document.querySelector(".shape");
+let btncircle = document.querySelector(".btncircle");
+let btnsquare = document.querySelector(".btnsquare");
+let btnrectangle = document.querySelector(".btnrectangle");
+
+// Add an event to each button
+btncircle.addEventListener("click", function () {
+    shape.className = "shape circle1";
+});
+
+btnsquare.addEventListener("click", function () {
+    shape.className = "shape square";
+});
+
+btnrectangle.addEventListener("click", function () {
+    shape.className = "shape rectangle";
+});
