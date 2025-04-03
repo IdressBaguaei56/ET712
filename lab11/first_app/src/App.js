f7bee1557b9b04d9e6e0cb5514ff402929d5895d
IdressBaguaei56 <Idress.Baguaei56@student.qcc.cuny.edu>
2025-04-03 19:38:23 +0000

    complete lab 11

--- lab11/first_app/src/App.js
+++ lab11/first_app/src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
+import yellowstoneimg from './images/yellowstone.jpg'
 
 function App() {
+  // variable
+  const msg = "Welcome to REACTJS"
+  //function
+  function printsomething(){
+    return "Hello from function"
+  }
   return (
     <div>
-      <h1> Idress Baguaei </h1>
+      <h1 style={{color:"magenta", textAlign:"center"}}> Idress Baguaei </h1>
       <p>This is my first REACTJS app</p>
+      <p> Bucket list </p>
+      <ol>
+        <li> It give you things to look forwad to </li>
+        <li> It makes life more meaningful </li>
+      </ol>
+      <h2>Inline styling</h2>
+      <label for="email">Enter e-mail: </label>
+      <input type= "text" id= "email" placeholder = "Enter a email address..." style ={{padding:"10px", backgroundColor:"lightgreen"}} />
+      <button type="submit" style={{backgroundColor:"crimson", padding:"10px 20px"}}>Submit</button>
+      <hr/>
+      <img src={yellowstoneimg} style={{width:"500px", display:"block", margin:"auto", border:"groove 10px orange"}}/>
+      <h2 className='subtitle'>Variables in JSX</h2>
+      <p> Calling variable msg = {msg}</p>
+      <p> Calling function printsomething = {printsomething()}</p>
     </div>
-
   );
 }
 
