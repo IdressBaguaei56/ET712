32559f53d66f4a1eb2902a93b38ba73a56c89b2a
IdressBaguaei56 <Idress.Baguaei56@student.qcc.cuny.edu>
2025-05-06 15:35:05 +0000

    finshed homework6

--- homework6/src/components/ProductListing.js
+++ homework6/src/components/ProductListing.js
@@ -3,48 +3,56 @@ import React from 'react';
 import ProductCard from './ProductCard';
 import './ProductListing.css';
 
+// Import images
+import airmax270 from '../images/nike1.jpg';
+import airforce1 from '../images/nike2.jpg';
+import dunklow from '../images/nike3.jpg';
+import jordan1 from '../images/nike4.jpg';
+import vaporfly from '../images/nike5.jpg';
+import blazer from '../images/nike6.jpg';
+
 function ProductListing() {
-  // Array of Nike shoe products
+  // Array of Nike shoe products with imported images
   const products = [
     {
       id: 1,
       name: "Nike Air Max 270",
-      image: "https://example.com/images/airmax270.jpg",
+      image: airmax270,
       price: 150,
       inStock: true
     },
     {
       id: 2,
       name: "Nike Air Force 1",
-      image: "https://example.com/images/airforce1.jpg",
+      image: airforce1,
       price: 100,
       inStock: true
     },
     {
       id: 3,
       name: "Nike Dunk Low",
-      image: "https://example.com/images/dunklow.jpg",
+      image: dunklow,
       price: 110,
       inStock: false
     },
     {
       id: 4,
       name: "Nike Air Jordan 1",
-      image: "https://example.com/images/jordan1.jpg",
+      image: jordan1,
       price: 180,
       inStock: true
     },
     {
       id: 5,
       name: "Nike ZoomX Vaporfly",
-      image: "https://example.com/images/vaporfly.jpg",
+      image: vaporfly,
       price: 250,
       inStock: false
     },
     {
       id: 6,
       name: "Nike Blazer Mid '77",
-      image: "https://example.com/images/blazer.jpg",
+      image: blazer,
       price: 105,
       inStock: true
     }
